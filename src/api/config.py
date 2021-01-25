e697791327df364d469cd9b0c27fbf1fa7c8599e
gordonshamway <stef.buchholz@googlemail.com>
2021-01-25 17:10:22 +0100

    updated migration file for correct deletion order

--- src/api/config.py
+++ src/api/config.py
@@ -7,11 +7,11 @@ class DevConfig(object):
     SECRET_KEY = 'do-i-really-need-this'
     FLASK_SECRET = SECRET_KEY
     DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
-    DB_NAME = 'corona'
+    DB_NAME = 'corona2'
     DB_USER = os.getenv('DB_USER', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
-    #DB_URI = os.getenv(DB_URI, 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME))
-    DB_URI='postgres://idvhfrtawyknop:35081e3febb224ec06731be10116283280342b6cd065da713cf26a40832081bc@ec2-34-195-169-25.compute-1.amazonaws.com:5432/da46k1e0drjj5l'
+    DB_URI = os.getenv('DB_URI', 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME))
+    #DB_URI='postgres://idvhfrtawyknop:35081e3febb224ec06731be10116283280342b6cd065da713cf26a40832081bc@ec2-34-195-169-25.compute-1.amazonaws.com:5432/da46k1e0drjj5l'
     SQLALCHEMY_DATABASE_URI = DB_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -20,6 +20,6 @@ class TestConfig(DevConfig):
     DB_USER = os.getenv('TEST_DB_USER', 'postgres')
     DB_PASSWORD = os.getenv('TEST_DB_PASSWORD', 'postgres')
     DB_HOST = os.getenv('TEST_DB_HOST', 'localhost:5432')
-    #DB_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
-    DB_URI='postgres://idvhfrtawyknop:35081e3febb224ec06731be10116283280342b6cd065da713cf26a40832081bc@ec2-34-195-169-25.compute-1.amazonaws.com:5432/da46k1e0drjj5l'
+    DB_URI = os.getenv('DB_URI', 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME))
+    #DB_URI='postgres://idvhfrtawyknop:35081e3febb224ec06731be10116283280342b6cd065da713cf26a40832081bc@ec2-34-195-169-25.compute-1.amazonaws.com:5432/da46k1e0drjj5l'
     SQLALCHEMY_DATABASE_URI = DB_URI
