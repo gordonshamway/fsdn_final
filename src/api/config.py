500f2bc98656e0f4f6766a44e329585b7e16b50d
gordonshamway <stef.buchholz@googlemail.com>
2021-01-25 16:55:10 +0100

    tried to overwrite the database path

--- src/api/config.py
+++ src/api/config.py
@@ -10,7 +10,8 @@ class DevConfig(object):
     DB_NAME = 'corona'
     DB_USER = os.getenv('DB_USER', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
-    DB_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+    #DB_URI = os.getenv(DB_URI, 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME))
+    DB_URI=postgres://idvhfrtawyknop:35081e3febb224ec06731be10116283280342b6cd065da713cf26a40832081bc@ec2-34-195-169-25.compute-1.amazonaws.com:5432/da46k1e0drjj5l
     SQLALCHEMY_DATABASE_URI = DB_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
